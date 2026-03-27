a199d54558a4c2cdde93a32c2c054fe6c2f0e089
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-27 15:16:45 +0530

    routes with query executed

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,11 @@ app=FastAPI()
 @app.get("/")
 def home():
     return {"message":"Fast Api running"}
+
+@app.get('/add')
+def add(a:int,b:int):
+    result =a=b
+    return {
+        "message":"the sum is :",
+        "result":result
+    }
