e1e52fb78d976701cce60b91cf6d6234c8ae4592
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-27 23:52:43 +0530

    lmabda demonstartion

--- backend/app.py
+++ backend/app.py
@@ -1,43 +1,43 @@
-class Ecart:
+# class Ecart:
 
-    users = [
-        {"name": "jeevan", "password": 1234},
-        {"name": "chintu", "password": 3451}
-    ]
+#     users = [
+#         {"name": "jeevan", "password": 1234},
+#         {"name": "chintu", "password": 3451}
+#     ]
 
-    def __init__(self, name, password):
-        self.name = name
-        self.password = password
+#     def __init__(self, name, password):
+#         self.name = name
+#         self.password = password
 
-    def login(self):
-        print("Login successful")
-        self.dashboard()
+#     def login(self):
+#         print("Login successful")
+#         self.dashboard()
 
-    def dashboard(self):
-        print(f"Welcome {self.name} to Ecart")
+#     def dashboard(self):
+#         print(f"Welcome {self.name} to Ecart")
 
-    def validation(self):
-        for user in self.users:
-            if user["name"] == self.name and user["password"] == self.password:
-                self.login()
-                return  
+#     def validation(self):
+#         for user in self.users:
+#             if user["name"] == self.name and user["password"] == self.password:
+#                 self.login()
+#                 return  
 
       
-        self.users.append({
-            "name": self.name,
-            "password": self.password
-        })
-        print("Account created")
-        data = [ele for ele in self.users if ele["name"] == self.name]
-        print(data)
-        self.login()
+#         self.users.append({
+#             "name": self.name,
+#             "password": self.password
+#         })
+#         print("Account created")
+#         data = [ele for ele in self.users if ele["name"] == self.name]
+#         print(data)
+#         self.login()
 
 
-name = input("Enter your name: ")
-password = int(input("Enter password: "))
+# name = input("Enter your name: ")
+# password = int(input("Enter password: "))
 
-k = Ecart(name, password)
-k.validation()
+# k = Ecart(name, password)
+# k.validation()
 
 
 
@@ -64,3 +64,6 @@ k.validation()
 
 # obj =app("chintu")
 # obj.validate()
+
+
+
