c3b19503c545cf887460b93ed0f0f7698b2a5df1
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-28 13:52:52 +0530

    based on clases

--- backend/app.py
+++ backend/app.py
@@ -42,28 +42,6 @@
 
 
 
-# class app:
-#     databse =["jeevan","chintu"]
-
-#     def __init__(self,name):
-#         self.name =name
-
-#     def login(self):
-#         print("log in succesfull")
-    
-#     def dashboard(self):
-#         print(f"this si {self.name}")
-
-#     def validate(self):
-#         if self.name in self.databse:
-#             self.login()
-#             self.dashboard()
-#         else:
-#             print("log in failed")
-        
-
-# obj =app("chintu")
-# obj.validate()
 
 
 
